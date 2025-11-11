186c127fcc777b4ba26a5d0e84443ff1bd767d1e
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2025-11-11 19:03:58 +0100

    added card-stack

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NgForOf, NgStyle } from "@angular/common";
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [],
+  imports: [NgForOf, NgStyle],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
