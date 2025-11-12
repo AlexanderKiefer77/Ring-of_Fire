f53a3d03209dd3185712ab611ae4de70b1991926
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2025-11-12 16:42:10 +0100

    modified function for show random card

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -28,11 +28,18 @@ export class GameComponent {
     // console.log(this.game); // anzeigen des gemischten stack
   }
 
-  takeCard() { 
-    this.currentCard = this.game.stack.pop(); // nimmt die letzte Karte aus dem stack
-    //console.log(this.currentCard); // anzeigen der letzten Karte, die aus dem stack genommen wurde
-    
-    this.pickCardAnimation = true;
+  takeCard() {
+    if (!this.pickCardAnimation) { // es kann nur der stack angeclickt werden, wenn die Variable = false ist (als Standart definiert)
+      this.currentCard = this.game.stack.pop(); // nimmt die letzte Karte aus dem stack
+      //console.log(this.currentCard); // anzeigen der letzten Karte, die aus dem stack genommen wurde
+
+      this.pickCardAnimation = true; // die Variable wird auf true gesetzt
+
+      setTimeout(() => { // nach 1,5 sek. wird die Variable wieder auf false gesetzt. Damit verschwindet nach 1,5 sek. die hingeworfene 
+                         // Karte und es kann erneut auf den stack geclickt werden.
+        this.pickCardAnimation = false;
+      }, 1500);
+    }
   }
 
 }
