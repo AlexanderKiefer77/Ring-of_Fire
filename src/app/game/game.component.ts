528d3a13dd5a8b5767d363c4e077808ae40aa840
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2025-11-12 16:19:18 +0100

    added function for create a random stackfrom cards for the game

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
 import { NgForOf, NgStyle, NgIf } from "@angular/common";
+import { Game } from '../../models/game';
+
 
 @Component({
   selector: 'app-game',
   standalone: true,
   imports: [NgForOf, NgStyle, NgIf],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent {
   pickCardAnimation = false;
+  game: Game; // ????
 
-  constructor() {}
+  constructor() {
+    this.game = new Game();
+  }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+    
+  }
 
   takeCard() {
     this.pickCardAnimation = true;
---
+++ src/models/game.ts
@@ -0,0 +1,34 @@
+
+export class Game { // erstellt ein Object für die Spieldaten
+    public players: string[] = [];
+    public stack: string[] = [];
+    public playedCards: string[] = [];
+    public currentPlayer: number = 0;
+
+    constructor() {
+        for(let i = 1; i < 14; i++) { // fügt die Spielkarten beim Start des Spieles in den stack hinzu
+            this.stack.push('spade_' + i);
+            this.stack.push('hearts_' + i);
+            this.stack.push('clubs_' + i);
+            this.stack.push('diamonds_' + i);
+        }
+        
+        shuffle(this.stack);
+    }
+}
+
+function shuffle(array: string[]): string[] {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (0!== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+
+    }
+
+    return array;
+}
