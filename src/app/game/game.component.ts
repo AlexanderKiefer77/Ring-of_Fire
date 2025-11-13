c8b79ee234c5512dd029cb4a5fa51bbe6f10df38
Alexander Kiefer <Alexander.Kiefer77@protonmail.com>
2025-11-13 15:33:12 +0100

    added button for add players

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { NgForOf, NgStyle, NgIf } from "@angular/common";
 import { Game } from '../../models/game';
 import { PlayerComponent } from '../player/player.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [NgForOf, NgStyle, NgIf, PlayerComponent],
+  imports: [NgForOf, NgStyle, NgIf, PlayerComponent, MatButtonModule, MatIconModule],
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss'],
 })
